dda9484151eb1482e5cb4f4ca35bd2f98e84dea1
Quanluis <quanluis@protonmail.com>
2019-10-14 21:22:58 -0400

    created a simple counter.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,38 @@ import './App.css';
 
 class App extends Component {
 
-  
+constructor(props){
+  super(props);
+    this.state = {
+      count: 0
+    }
+}
+
+increment = () => {
+  this.setState({
+    count: this.state.count  + 1
+  })
+}
+
+decrement = () => {
+  this.setState({
+    count: this.state.count - 1
+  })
+}
+
+
+sayHello(){
+  console.log('Hello world')
+}
+
 
 render(){
   return (
     <div>
       <h1>Hello</h1>
-      <button>Switch Name</button>
+      <button onClick = {this.increment} >+</button>
+      <p>Count : {this.state.count}</p>
+      <button onClick = {this.decrement}>-</button>
       <Person name = "Max" age = '19' />
       <Person name = "Bob" age = '23'>My hobbies: skating </Person>
     </div>
